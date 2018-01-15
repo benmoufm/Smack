5889924ad1129c2328629743f9ad65a610c01c81
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-15 17:50:04 +0100

    Exit create account view controller to channel view controller

--- Smack/Controller/CreateAccountViewController.swift
+++ Smack/Controller/CreateAccountViewController.swift
@@ -16,4 +16,7 @@ class CreateAccountViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
 }
