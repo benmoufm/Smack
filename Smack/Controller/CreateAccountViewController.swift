c39477e4235e9f8152c3a83ab81b7a6af535f434
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-15 17:23:00 +0100

    Add create account view controller

---
+++ Smack/Controller/CreateAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountViewController.swift
+//  Smack
+//
+//  Created by Mélodie Benmouffek on 15/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
