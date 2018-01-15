8e85337c01eecab9181604fcf4aa5b64a89b8766
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-15 17:09:27 +0100

    Add login view controller and close button action

---
+++ Smack/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Smack
+//
+//  Created by Mélodie Benmouffek on 15/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
