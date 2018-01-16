fa23da1c5fb919819577c4e3d60818f52a53b36c
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-16 18:11:10 +0100

    Add action anthenticate on login button pressed

--- Smack/Controller/CreateAccountViewController.swift
+++ Smack/Controller/CreateAccountViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,4 +25,27 @@ class CreateAccountViewController: UIViewController {
     @IBAction func closeButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
+
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTextField.text, emailTextField.text != "" else {
+            return
+        }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else {
+            return
+        }
+
+        AuthentificationService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered user")
+            }
+        }
+    }
+
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+
+    @IBAction func pickBackgroundColorPressed(_ sender: Any) {
+    }
+    
+
 }
