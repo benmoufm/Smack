ed2da999422c311bff39c2da06276e186aadab34
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-17 18:03:54 +0100

    Add notification observer to channel view controller

--- Smack/Controller/ChannelViewController.swift
+++ Smack/Controller/ChannelViewController.swift
@@ -12,15 +12,22 @@ class ChannelViewController: UIViewController {
 
     // Outlets
     @IBOutlet weak var loginButton: UIButton!
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ChannelViewController.userDataDidChange(_:)),
+                                               name: NOTIF_USER_DATA_DID_CHANGE,
+                                               object: nil)
     }
 
     @IBAction func loginButtonPressend(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
 
-    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
+    @objc func userDataDidChange(_ notification: Notification) {
+
+    }
 }
