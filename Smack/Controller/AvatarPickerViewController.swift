53fce103fc216a12d5551e353029fd70187b331b
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-17 17:23:17 +0100

    Setup dark and light avatars

--- Smack/Controller/AvatarPickerViewController.swift
+++ Smack/Controller/AvatarPickerViewController.swift
@@ -15,6 +15,9 @@ class AvatarPickerViewController: UIViewController, UICollectionViewDelegate,
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
 
+    // Variables
+    var avatarType = AvartarType.dark
+
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
@@ -27,6 +30,7 @@ class AvatarPickerViewController: UIViewController, UICollectionViewDelegate,
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCollectionViewCell {
+            cell.configureCell(index: indexPath.item, avatarType: avatarType)
             return cell
         }
         return AvatarCollectionViewCell()
@@ -36,7 +40,25 @@ class AvatarPickerViewController: UIViewController, UICollectionViewDelegate,
         return 1
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numberOfColumns: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numberOfColumns = 4
+        }
+        let spaceBetweenCells: CGFloat = 10
+        let padding: CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numberOfColumns - 1) * spaceBetweenCells)
+            / numberOfColumns
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+
     @IBAction func segmantedControlChanged(_ sender: Any) {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        } else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
     }
 
     @IBAction func backButtonPressed(_ sender: Any) {
