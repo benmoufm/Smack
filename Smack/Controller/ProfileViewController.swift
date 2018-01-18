de8ea83bfda65620a9b9dfbf900dee9ad62cdcde
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-18 10:13:15 +0100

    Draw xib for profile view controller

---
+++ Smack/Controller/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Smack
+//
+//  Created by Mélodie Benmouffek on 18/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
