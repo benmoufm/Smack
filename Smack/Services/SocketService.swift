d86f6b66b92793d7b453090b2a67643b473afee6
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-18 12:19:21 +0100

    Add socket service to establish and close connections to the server

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,28 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by Mélodie Benmouffek on 18/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+    static let instance = SocketService()
+
+    override init() {
+        super.init()
+    }
+
+    var socket: SocketIOClient = SocketManager(socketURL: URL(string: BASE_URL)!).defaultSocket
+
+    func establishConnection() {
+        socket.connect()
+    }
+
+    func closeConnection() {
+        socket.disconnect()
+    }
+}
