746edd202480bb9fc940d0ae3ce61c46e2b02348
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-18 11:59:48 +0100

    Add IBOutlets and actions to add channel view controller

--- Smack/Controller/AddChannelViewController.swift
+++ Smack/Controller/AddChannelViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class AddChannelViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var backgroundView: UIView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func closeButtonPressed(_ sender: Any) {
+
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func createChannelButtonPressed(_ sender: Any) {
+        
     }
-    */
-
 }
