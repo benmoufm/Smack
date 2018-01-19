2222882b5af65dd7ce1fba3b1b317ab05e37ea62
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-19 09:45:06 +0100

    Create message model

---
+++ Smack/Model/MessageModel.swift
@@ -0,0 +1,19 @@
+//
+//  MessageModel.swift
+//  Smack
+//
+//  Created by Mélodie Benmouffek on 19/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct MessageModel {
+    private(set) var messageBody: String!
+    private(set) var userName: String!
+    private(set) var channelId: String!
+    private(set) var userAvatar: String!
+    private(set) var userAvatarColor: String!
+    private(set) var id: String!
+    private(set) var timeStamp: String!
+}
